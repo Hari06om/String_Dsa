84bcc2b31f852e323e50d58c4f470e83e51cd155
Hari06om <hariohmverma96@gmail.com>
2025-09-17 10:16:24 +0530

    lean the concept of the palindrome  form  a string.

---
+++ palindrome.java
@@ -0,0 +1,20 @@
+import  java.util.*;
+
+public class palindrome {
+
+    public static boolean isPalindrome(String str){
+        int n = str.length();
+        for(int i=0;i<n;i++){
+            if(str.charAt(i)!= str.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.print(isPalindrome(str));
+    }
+}
